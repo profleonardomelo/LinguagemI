8240b17992af5a31505e9bc81fc9d823f4e82195
Prof. Leonardo Melo <90953714+profleonardomelo@users.noreply.github.com>
2022-11-28 17:06:49 -0300

    Atualizando a classe ProgramaEntradaDeDadosPeloTeclado

---
+++ LinguagemI/src/Java_10/ProgramaEntradaDeDadosPeloTecladoScanner.java
@@ -0,0 +1,48 @@
+//Define o pacote que contém esta classe
+package Java_10;
+
+//Importando Classes
+import java.util.*;
+
+//Cria uma classe pública chamada "ProgramaEntradaDeDadosPeloTecladoScanner"
+public class ProgramaEntradaDeDadosPeloTecladoScanner {
+
+    //Cria o método principal (função) "main"
+    public static void main(String[] args) {
+        
+        //Imprime na tela um texto e coloca a cursor em uma nova linha
+        System.out.println("  <<< Progrma Soma >>>>  ");
+        
+        //Cria um objeto leitor de dados do tipo Scanner
+        Scanner leitor = new Scanner(System.in);
+        
+        //Imprime na tela um texto e coloca a cursor em uma nova linha
+        System.out.println("Entre com o seu nome: ");
+        
+        //Lê do teclado um texto digitado, após ser pressionada a tecla ENTER
+        String n = leitor.next();
+        
+        //Imprime na tela um texto e coloca a cursor em uma nova linha
+        System.out.println("Entre com primeiro numero: ");
+        
+        //Lê do teclado um texto digitado e convert para INT, após ser pressionada a tecla ENTER
+        int x = leitor.nextInt();
+        
+        //Imprime na tela um texto e coloca a cursor em uma nova linha
+        System.out.println("Entre com segundo numero: ");
+        
+        //Lê do teclado um texto digitado e convert para INT, após ser pressionada a tecla ENTER
+        int y = leitor.nextInt();
+        
+        //Soma duas variáveis do tipo INT e atribui o resultado dessa soma a uma terceira variável
+        int soma = x + y;
+        
+        //Imprime na tela um texto concatenado com uma variável e coloca a cursor em uma nova linha
+        System.out.println("O resultado da soma e: " + soma);
+        
+        //Imprime na tela um texto concatenado com uma variável e coloca a cursor em uma nova linha
+        System.out.println("Autor(a) do program: " + n);
+        
+    }
+    
+}
---
+++ LinguagemI/src/Java_11/ProgramaEntradaESaidaDeDadosComCaixaDeDialogo.java
@@ -0,0 +1,12 @@
+//Define o pacote que contém esta classe
+package Java_11;
+
+//Cria uma classe pública chamada "ProgramaEntradaESaidaDeDadosComCaixaDeDialogo"
+public class ProgramaEntradaESaidaDeDadosComCaixaDeDialogo {
+
+    //Cria o método principal (função) "main"
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
