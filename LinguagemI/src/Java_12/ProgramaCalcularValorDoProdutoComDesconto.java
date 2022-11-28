2f993d99ead778b7a38e1e39b2a202cb195a3c18
Prof. Leonardo Melo <90953714+profleonardomelo@users.noreply.github.com>
2022-11-28 17:48:07 -0300

    Atualizar ProgramaClacularValorDoProdutoComDesconto

---
+++ LinguagemI/src/Java_12/ProgramaCalcularValorDoProdutoComDesconto.java
@@ -0,0 +1,22 @@
+/*Usando a classe Scanner para entrada de dados, 
+crie uma classe que receba o valor de um produto e a 
+porcentagem de desconto, calcule e mostre o valor do desconto 
+e o valor do produto com o desconto. Observação: o valor do 
+desconto é calculado por meio da fórmula: 
+valor do desconto = valor do produto * percentual de desconto/100.
+*/
+
+//Define o pacote que contém esta classe
+package Java_12;
+
+//Cria uma classe pública chamada "ProgramaCalcularValorDoProdutoComDesconto"
+public class ProgramaCalcularValorDoProdutoComDesconto {
+
+    //Cria o método principal (função) "main"
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
---
+++ LinguagemI/src/Java_13/ProgramaCalcularMediaDeNotas.java
@@ -0,0 +1,25 @@
+/*Usando a classe JOptionPane para entrada de dados,
+elabore uma classe que receba a nota de duas provas
+e de um trabalho. Calcule e mostre a média e o 
+resultado final (aprovado ou reprovado). 
+Para calcular a média utilize a fórmula seguinte: 
+média = (nota da prova 1 + nota da prova 2 
++ nota do trabalho) / 3. 
+Considere que a média mínima para aprovação é 6.
+Obs.: Não usar comando if.
+*/
+
+
+//Define o pacote que contém esta classe
+package Java_13;
+
+//Cria uma classe pública chamada "ProgramaCalcularMediaDeNotas"
+public class ProgramaCalcularMediaDeNotas {
+
+    //Cria o método principal (função) "main"
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
