cdc0ca4dbd97227986bd2a52e2a8a25a3661537b
Prof. Leonardo Melo <90953714+profleonardomelo@users.noreply.github.com>
2022-12-03 19:13:35 -0300

    Update ProgramaExercicioCalcularValorDoProdutoComDesconto.java

--- LinguagemI/src/Java_12_ProgramaExercicioCalcularValorDoProdutoComDesconto/ProgramaExercicioCalcularValorDoProdutoComDesconto.java
+++ LinguagemI/src/Java_12_ProgramaExercicioCalcularValorDoProdutoComDesconto/ProgramaExercicioCalcularValorDoProdutoComDesconto.java
@@ -18,26 +18,34 @@ public class ProgramaExercicioCalcularValorDoProdutoComDesconto {
     //Cria o método principal (função) "main"
     public static void main(String[] args) {
         
+        //Exibe na Tela String com título do programa
         System.out.println("<<<< Programa Calculo de Desconto de Produto >>>>\n");
         
+        //Cria um objeto com nome leitor, do tipo Scanner 
         Scanner leitor = new Scanner(System.in);
         
+        //Exibe na Tela String solicitando que seja digitado dado numérico
         System.out.print("Digite o Valor do Produto: ");
         
+        //Lê do teclado um dado do tipo string e convert o dado lido para float
         float valorDoProduto = leitor.nextFloat();
         
+        //Exibe na Tela String solicitando que seja digitado dado numérico
         System.out.print("Digite o Percentual de Desconto: ");
         
+        //Lê do teclado um dado do tipo string e convert o dado lido para float
         float percentualDeDesconto = leitor.nextFloat();
         
-        
+        //Calcula o valor do desconto do produto e armazana o resultado em uma variável do tipo float
         float valorDoDesconto = valorDoProduto * percentualDeDesconto / 100;
         
+        //Exibe na Tela String com o valor do desconto do produto
         System.out.println("Valor do Desconto: " + valorDoDesconto);
         
-        
+        //Calcula o valor do produto com desconto e armazana o resultado em uma variável do tipo float
         float valorDoProdutoComDesconto = valorDoProduto - valorDoDesconto;
         
+        //Exibe na Tela String com o valor do produto com desconto
         System.out.println("Valor do Produto com Desconto: " + valorDoProdutoComDesconto);
     }
     
