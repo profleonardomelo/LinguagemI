40e45621e342cf7bc5ca81e651f50a18592fb8b8
Prof. Leonardo Melo <90953714+profleonardomelo@users.noreply.github.com>
2022-12-04 16:30:40 -0300

    Adição de classes com exemplos de comando For

---
+++ LinguagemI/src/Java_17_ProgramaEstruturasDeRepeticaoFor/ProgramaEstruturasDeRepeticaoFor.java
@@ -0,0 +1,20 @@
+package Java_17_ProgramaEstruturasDeRepeticaoFor;
+
+public class ProgramaEstruturasDeRepeticaoFor {
+
+    public static void main(String[] args) {
+       
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
+        }
+        
+        System.out.println();
+        
+        for (int i = 1; i <= 10; i += 2) {
+            System.out.print(i + " ");
+        }
+        
+        System.out.println("\nAcabou!");
+    }
+    
+}
