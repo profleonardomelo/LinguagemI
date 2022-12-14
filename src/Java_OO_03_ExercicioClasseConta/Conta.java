1233f5f31b9c3fa2e25421142d753fb897f68cfd
Prof. Leonardo Melo <90953714+profleonardomelo@users.noreply.github.com>
2022-12-14 18:52:36 -0300

    Exercício Criação de Classe

---
+++ src/Java_OO_03_ExercicioClasseConta/Conta.java
@@ -0,0 +1,18 @@
+/*
+Crie uma classe pública chamada Conta que possui os
+atributos públicos: número (int), saldo (double) e limite (double).
+Além disso, a classe Conta deve possuir os métodos: Sacar,
+Depositar e MostrarInformacoes.
+O método Sacar rebece como parâmetro de entrada um valor a ser sacado
+e realiza a operação de saque.
+O método Depositar rebece como parâmetro de entrada um valor a ser depositado
+e realiza a operação de depósito.
+O método MostrarInformacoes não possui parâmetros de entrada e apenas
+imprime na tela (console) as informações contidas nos atributos.
+ */
+
+package Java_OO_03_ExercicioClasseConta;
+
+public class Conta {
+
+}
---
+++ src/Java_OO_03_ExercicioClasseConta/ProgramaUsarConta.java
@@ -0,0 +1,5 @@
+package Java_OO_03_ExercicioClasseConta;
+
+public class ProgramaUsarConta {
+
+}
