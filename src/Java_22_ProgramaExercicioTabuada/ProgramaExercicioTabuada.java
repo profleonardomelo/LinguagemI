465bf95eb15df4e2f85bf2c8a3b2a178be4e8d1f
Prof. Leonardo Melo <90953714+profleonardomelo@users.noreply.github.com>
2022-12-27 15:04:04 -0300

    resolução do exercício de tabuada

--- src/Java_22_ProgramaExercicioTabuada/ProgramaExercicioTabuada.java
+++ src/Java_22_ProgramaExercicioTabuada/ProgramaExercicioTabuada.java
@@ -7,11 +7,22 @@ de todos os cálculos de 1 a 10. Ver exemplo de execução no arquivo
 
 package Java_22_ProgramaExercicioTabuada;
 
+import javax.swing.JOptionPane;
+
 public class ProgramaExercicioTabuada {
 
     public static void main(String[] args) {
 
-        
+        String strNumero = JOptionPane.showInputDialog("Forneça um número: ");
+        int numero = Integer.parseInt(strNumero);
+
+        String mensagem = "";
+
+        for (int i = 1; i <= 10; i++) {
+            mensagem += numero + " x " + i + " = " + (numero * i) + "\n";
+        }
+
+        JOptionPane.showMessageDialog(null, mensagem);
     }
     
 }
